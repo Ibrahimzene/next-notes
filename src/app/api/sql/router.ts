55903239d575f8c358da037dabab0fa35a25525c
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2024-01-29 11:03:19 +0100

    Add Notes table to SQL database

--- src/app/api/sql/router.ts
+++ src/app/api/sql/router.ts
@@ -6,7 +6,8 @@ export async function GET(request: Request) {
 		if (process.env.APP_ENV !== 'development') throw Error('not authorized');
 		
 		// CREATE NOTES TABLE
-		await sql`CREATE TABLE IF NOT EXISTS Notes ( id)`
+		await sql`CREATE TABLE IF NOT EXISTS Notes ( id SERIAL PRIMARY KEY, BODY VARCHAR(255), rank DECIMAL);`;
+		return NextResponse.json('ok', { status: 200});
 	}
 	catch (error: any) {
 		return NextResponse.json(error.message, { status: 500 });
